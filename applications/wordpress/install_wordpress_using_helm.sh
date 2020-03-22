949eabc08b0c806336b972e13ca99a2c6bce9e71
haofan-ms <hafa@microsoft.com>
2020-03-22 15:53:34 -0700

    update nodeSelector for helm 2

--- applications/wordpress/install_wordpress_using_helm.sh
+++ applications/wordpress/install_wordpress_using_helm.sh
@@ -29,7 +29,7 @@ echo "Update helm repository..."
 helm repo update
 
 echo "Install wordpress..."
-helm install stable/wordpress --set wordpressSkipInstall=false,nodeSelector."beta\\.kubernetes\\.io/os"=linux
+helm install stable/wordpress --set wordpressSkipInstall=false --node-selectors "beta.kubernetes.io/os"="linux"
 
 echo "Done with installation, checking release status..."
 wpRelease=$(helm ls -d -r | grep -i 'DEPLOYED\(.*\)wordpress' | grep -Eo '^[a-z,-]+')
