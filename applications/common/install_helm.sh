d385c671cd1e3c029c325dadb34fcef3f52bbad0
haofan-ms <hafa@microsoft.com>
2020-03-29 22:23:18 -0700

    update

--- applications/common/install_helm.sh
+++ applications/common/install_helm.sh
@@ -31,7 +31,7 @@ if [[ -z $helmcmd ]]; then
     #helm version
     #helm init || true
     
-    curl curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
     chmod 700 get_helm.sh
     ./get_helm.sh --version ${HELM_VERSION}
     
